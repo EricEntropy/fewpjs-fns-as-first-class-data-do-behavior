2c5e74ac3ea7f1171e6f52d3e5073f02fd1809f3
eric <contreras.eric18@gmail.com>
2021-07-19 15:41:31 -0400

    commit tree 08c0ae7ea5780ab57c7d7ca04ee85b114271c417

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time) {
+  let hourArray = time.split(":")
+  let hour = parseInt(hourArray)
+
+  if (hour < 12){
+    return "Good Morning";
+  }
+  else if (hour >= 12 && hour <= 17){
+    return "Good Afternoon";
+  }
+  else if (hour > 17){ 
+    return "Good Evening";
+  }
+}
+
+function displayMessage(msg) {
+  const content = document.getElementById("greeting");
+  content.innerHTML = msg
+}
